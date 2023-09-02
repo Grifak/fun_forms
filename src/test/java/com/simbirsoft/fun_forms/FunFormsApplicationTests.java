afc4e5451f65a5676121fd006a6a74f513132c0d
vadim.nepomnyashchii <vadim.nepomnyashchii@simbirsoft.com>
2023-09-02 13:33:37 +0300

    initial

---
+++ src/test/java/com/simbirsoft/fun_forms/FunFormsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simbirsoft.fun_forms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FunFormsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
